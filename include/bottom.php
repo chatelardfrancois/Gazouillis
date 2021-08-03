f2d2369a8cfd7e2f34e34c06b9bd9987cbfc6b72
chatelardfrancois <f.chatelard@hotmail.fr>
2021-08-03 15:07:32 +0200

    modification url

--- include/bottom.php
+++ include/bottom.php
@@ -5,7 +5,7 @@
                 window.location = 'index.php';
             }
         </script>
-        <i onclick="GoToHomePage()" <?= (($_SERVER['REQUEST_URI'] === "/Gazouillis/index.php") or ($_SERVER['REQUEST_URI'] === "/Gazouillis/")) ? "style='color:rgb(29, 161, 242);'" : "" ?>class="fas fa-home"></i>
+        <i onclick="GoToHomePage()" <?= (($_SERVER['REQUEST_URI'] === "/ancien/Gazouillis/index.php") or ($_SERVER['REQUEST_URI'] === "/ancien/Gazouillis/")) ? "style='color:rgb(29, 161, 242);'" : "" ?>class="fas fa-home"></i>
         <i class="fas fa-search"></i>
         <i class="far fa-envelope"></i>
     </div>
